e78cac37682466eebff019e1f21b9668fbb8882e
dimkaar <dimkaar1102@gmail.com>
2019-02-26 11:50:17 +0300

    refactor(package.json, package-lock.json, webpack.congig.js): babel has been deleted

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,6 @@ module.exports = {
     path: path.join(__dirname, `public`)
   },
   devtool: `source-map`,
-  module: {
-    rules: [{
-      test: /\.js$/,
-      use: `babel-loader`
-    }]
-  },
   devServer: {
     contentBase: path.join(__dirname, `public`),
     publicPath: `http://localhost:8080/`,
